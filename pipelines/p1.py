ca5703192a608992cd221fd98ed97cfb1e082836
somani.raghav@prophecy.io <somani.raghav@prophecy.io>
2025-08-22 12:11:10 +0000

    c1

---
+++ pipelines/p1.py
@@ -0,0 +1,30 @@
+Schedule = Schedule(cron = "* 0 2 * * * *", timezone = "GMT", emails = ["email@gmail.com"], enabled = False)
+SensorSchedule = SensorSchedule(enabled = False)
+
+with DAG(Schedule = Schedule, SensorSchedule = SensorSchedule):
+    p1__DataCleansing_1 = Task(task_id = "p1__DataCleansing_1", component = "Model", modelName = "p1__DataCleansing_1")
+    p1__Aggregate_1 = Task(task_id = "p1__Aggregate_1", component = "Model", modelName = "p1__Aggregate_1")
+    OrchestrationSource_0 = SourceTask(
+        task_id = "OrchestrationSource_0", 
+        component = "OrchestrationSource", 
+        kind = "S3Source", 
+        connector = Connection(kind = "s3"), 
+        isNew = True, 
+        format = CSVFormat(
+          allowLazyQuotes = False, 
+          allowEmptyColumnNames = True, 
+          separator = ",", 
+          nullValue = "", 
+          encoding = "UTF-8", 
+          header = True
+        ), 
+        fileOperationProperties = {"fileLoadingType" : "filepath", "includeFileNameColumn" : True}
+    )
+    OrchestrationSource_0 = Task(
+        task_id = "OrchestrationSource_0", 
+        component = "Dataset", 
+        label = "OrchestrationSource_0", 
+        table = {"name" : "{{ prophecy_tmp_source('p1', 'OrchestrationSource_0') }}", "sourceType" : "UnreferencedSource"}
+    )
+    OrchestrationSource_0.out >> OrchestrationSource_0.input_port_1
+    OrchestrationSource_0.output_port_1 >> p1__Aggregate_1.in_0
